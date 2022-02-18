9b87ac4d1c166c9fd43e9c95df331b1f0e0c437f
yuukiuvw <98954743+yuukiuvw@users.noreply.github.com>
2022-02-18 21:44:56 +0900

    issue6 改訂

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,20 +34,17 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => wdaysから値を取り出す記述}
-      @week_days.push(days)
-    end
 
-    wday_num = # wdayメソッドを用いて取得した数値
+    wday_num = Date.today.wday + x
     if wday_num >=7
       wday_num = wday_num -7
     end
 
-    days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays[Date.today.wday]
-
+    days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays[wday_num]}
+      @week_days.push(days)
   end
 end
-#曜日を出す　ゴール
-#曜日を出すためには？　wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']を使う
-#上のものを使ってどのように曜日を表示させるか　 wdays[数字か変数]
-#
+end
+#曜日を出すゴール
+#曜日を出すためには？wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']を使う
+#上のものを使ってどのように曜日を表示させるか wdays[数字か変数]
