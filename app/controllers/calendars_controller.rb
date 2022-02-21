116c493090718f41d6cf1c8acfc73f03542df7f1
yuukiuvw <98954743+yuukiuvw@users.noreply.github.com>
2022-02-21 11:34:43 +0900

    再修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -35,19 +35,15 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
-
     wday_num = Date.today.wday + x
     if wday_num >=7
       wday_num = wday_num -7
-
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
-      @week_days.push(days)
     end
 
-    days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday: wdays[wday_num]}
-      @week_days.push(days)
+      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday: wdays[wday_num]}
+        @week_days.push(days)
+    end
   end
-end
 end
 #曜日を出すゴール
 #曜日を出すためには？wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']を使う
