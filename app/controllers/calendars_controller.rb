b2109b20fdeaddac50f7f88850f419e77771b3ce
yuukiuvw <98954743+yuukiuvw@users.noreply.github.com>
2022-02-21 10:34:12 +0900

    ハッシュ型をシンボル型へ変更

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -44,7 +44,7 @@ class CalendarsController < ApplicationController
       @week_days.push(days)
     end
 
-    days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays[wday_num]}
+    days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday: wdays[wday_num]}
       @week_days.push(days)
   end
 end
